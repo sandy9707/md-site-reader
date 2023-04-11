d48c8e36c494caa957d5b62851576da5ca96d33b
sandy9707 <Sandy970701@Gmail.com>
2023-04-11 10:05:54 +0800

    2023-04-11 10-05-54

--- config.js
+++ config.js
@@ -9,7 +9,16 @@ config.lang = 'zh'
 config.langs = ['en', 'ja', 'pt-br', 'zh']
 
 config.docProject = 'astaxie/build-web-application-with-golang'
-config.project = 'astaxie/build-web-application-with-golang'
+// e.g https://raw.githubusercontent.com/{astaxie/build-web-application-with-golang}/master/{beanfactory.md}
+config.dataUrl = 'https://raw.githubusercontent.com/astaxie/build-web-application-with-golang/master/en/'
+config.docUrl = 'https://github.com/astaxie/build-web-application-with-golang'
+//   // e.g https://github.com/astaxie/build-web-application-with-golang/edit/master/http.md
+config.editUrl = 'https://github.com/astaxie/build-web-application-with-golang/edit/master'
+
+config.project = 'sandy9707/md-site-reader'
+config.projectUrl = 'https://github.com/sandy9707/md-site-reader'
+config.issueUrl = 'https://github.com/astaxie/build-web-application-with-golang/issues'
+
 
 config.authorPage = 'https://github.com/astaxie'
 config.authorName = 'astaxie'
